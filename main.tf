2b20fa703e8d72c76b9c7a1ff813b4942104f37f
Archana Pradhan <121005270+archana-202@users.noreply.github.com>
2025-04-17 00:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "eu-north-1"
 resource "aws_instance" "Archana" {
 count = 4
 ami = "ami-0a2370e7c0f21e179"
-instance_type = "t2.medium"
+instance_type = "t3.medium"
 key_name = "projectnetflix"
 vpc_security_group_ids = ["sg-07b63c988c02f9f74"]
 tags = {
